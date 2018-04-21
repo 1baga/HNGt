256af893edef4e4ff8baba092eccb16a914faeb0
Umar Mash <mafulun4real@gmail.com>
2018-04-21 15:28:04 +0100

    First commit to github

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>HNG | Stage 1</title>
+		
+		<link href="css/main.css" rel=stylesheet />
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container" >
+			<p id="inte">HNG Internship</p>
+			<p id="time"><?php echo date("h:i A"); ?></p>
+			<p id="date" ><?php echo date("l jS \of F Y") ?></p>
+			<img src="img/logo.png" alt="Image" id="hot" />
+		</div>
+	</body>
+</html>
